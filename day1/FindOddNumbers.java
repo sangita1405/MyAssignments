2bfe39ebdadd2e295969acf209df9b5d2458bf68
sangita1405 <162820532+sangita1405@users.noreply.github.com>
2024-03-09 21:24:08 +0530

    Add files via upload
    Day 1

---
+++ day1/FindOddNumbers.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class FindOddNumbers {
+public static void main(String[] args) {
+	int number = 10;
+	for (int i = 1; i < number; i++) {
+		if(i%2==1)
+			System.err.println(i);
+		
+	}
+}
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Firefox {
+	public static void main(String[] args) {
+		float browserVersion  = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		char browserLogo = 'f';
+		int releaseYear = 1998;
+		
+		System.out.println("Browser Version is: "+browserVersion);
+		System.out.println("Browser Name is: "+browserName);
+		System.out.println("is visible is: "+isVisible);
+		System.out.println("browser Logo is: "+browserLogo);
+		System.out.println("releaseYear is: "+releaseYear);
+		
+		
+		
+		
+	
+			
+	}
+
+}
